53085463667afaeb95f4c10529a368f63fd04508
Gabriel Salustiano <gabriel.salustiano.peres@gmail.com>
2025-05-12 14:54:39 -0300

    app incompleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import BombSelector from './components/BombSelector';
+import PlaneSelector from './components/PlaneSelector';
+import Results from './components/Results';
+
+import { bombs } from './data/bombs';
+import { planes } from './data/planes';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedBomb, setSelectedBomb] = useState(null);
+  const [selectedPlane, setSelectedPlane] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div ClassName = "App">
+      <h1> Payload Calculator </h1>
+
+      <BombSelector
+        bombs={bombs}
+        selectedBomb={selectedBomb}
+        onChange={setSelectedBomb}
+      />
+
+      <PlaneSelector
+      planes={planes}
+      selectedPlane={selectedPlane}
+      onChange={setSelectedPlane}
+      />
+
+      <Results
+      bomb={selectedBomb}
+      plane={selectedPlane}
+      bombsData={bombs}
+      />
+    </div>
   )
 }
 
-export default App
+export default App;
